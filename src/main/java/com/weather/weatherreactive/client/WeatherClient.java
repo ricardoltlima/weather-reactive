99a53937a938974526ae201219a7da1d2a10cd1b
Ricardo Lima <ricardoltlima@gmail.com>
2026-09-14 20:56:03 -0400

    Deleted unnecessary imports

--- src/main/java/com/weather/weatherreactive/client/WeatherClient.java
+++ src/main/java/com/weather/weatherreactive/client/WeatherClient.java
@@ -2,8 +2,6 @@ package com.weather.weatherreactive.client;
 
 import com.weather.weatherreactive.dto.WeatherResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
